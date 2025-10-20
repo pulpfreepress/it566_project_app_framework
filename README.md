464c079826706c696e5a210ede1557ca7ee4f26e
Rick Miller <rick@pulpfreepress.com>
2025-10-20 15:31:39 -0400

    Initial commit.

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # it566_project_app_framework
 Application framework for semester project.
+
+## Instructions
+
+- Create your project GitHub repository
+- Clone your repository to your local machine
+- Copy the contents of the app_framework directory to your repository directory
+- Begin work on your project
+
+
---
+++ app_framework/src/main.py
@@ -0,0 +1,37 @@
+"""Entry point for the Employee Training Application."""
+
+import json
+from argparse import ArgumentParser
+from application_name.persistence_layer.mysql_persistence_wrapper \
+	import MySQLPersistenceWrapper
+
+
+def main():
+	"""Entry point."""
+	args = configure_and_parse_commandline_arguments()
+
+	if args.configfile:
+		config = None
+		with open(args.configfile, 'r') as f:
+			config = json.loads(f.read())
+			
+		db = MySQLPersistenceWrapper(config)
+
+
+def configure_and_parse_commandline_arguments():
+	"""Configure and parse command-line arguments."""
+	parser = ArgumentParser(
+	prog='main.py',
+	description='Start the application with a configuration file.',
+	epilog='POC: Your Name | your@email')
+
+	parser.add_argument('-c','--configfile',
+					help="Configuration file to load.",
+					required=True)
+	args = parser.parse_args()
+	return args
+
+
+
+if __name__ == "__main__":
+	main()
